2a5017c7e4a2cf7c3b021d3928e398827fa60cb6
Alex <alis_mal@mail.ru>
2023-01-15 11:13:36 +0300

    Add commit end coding task 44
    :wq

---
+++ Sem_1_task_4/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Принимаем целое трехзначное число. 
+выводим последнюю  его цифруло 
+*/
+
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+Console.WriteLine(number % 10);
