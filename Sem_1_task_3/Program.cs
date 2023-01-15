74621fbe74187b8d13f0a085ffdd293ec4a0fae9
Alex <alis_mal@mail.ru>
2023-01-15 11:07:39 +0300

    add program 3 :qw

---
+++ Sem_1_task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿/* Принимаем целое число. ввыодим целые числа от -число до + число 
+*/
+// Дни недели по запросу цифрой
+Console.WriteLine("Введите число");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+ for (int i = -number; i <= number; i++)
+ {
+    Console.Write(i + " ");
+ }
