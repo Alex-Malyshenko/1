af61eb28ec4d1702f2d57149b916e061efcddbbc
Alex <alis_mal@mail.ru>
2023-01-15 10:33:54 +0300

    Add two program :wq

--- Sem_1_days_week/Program.cs
+++ Sem_1_days_week/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Дни недели по запросу цифрой
+Console.WriteLine("Введите номер дня недели");
+string numberStr = Console.ReadLine();
+int number = Convert.ToInt32(numberStr);
+
+if (number == 1)
+
+{
+    Console.WriteLine("Он называется : Понедельник");
+}
+ 
+if (number == 2)
+
+{
+    Console.WriteLine("Он называется : Вторник");
+}
+ if (number == 3)
+
+{
+    Console.WriteLine("Он называется : Среда");
+}
+ if (number == 4)
+
+{
+    Console.WriteLine("Он называется : Четверг");
+}
+ 
+if (number == 5)
+
+{
+    Console.WriteLine("Он называется : Пятница");
+}
+ if (number == 6)
+
+{
+    Console.WriteLine("Он называется : Суббота");
+}
+ if (number == 7)
+
+{
+    Console.WriteLine("Он называется : Воскресенье");
+}
+ 
