ea5852a69632adaf131f5a4c8218e86b7a94268f
Alex <alis_mal@mail.ru>
2023-01-17 22:55:44 +0300

    ADD HOME FOLDER  WORK 2

--- HOME_WORK/HOME_WORK_4/Program.cs
+++ HOME_WORK/HOME_WORK_4/Program.cs
@@ -7,7 +7,7 @@
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 2; i <= number; i+=2)
+for (int i = 2; i <= number; i += 2)
 {
-    Console.Write(i+ ", ");
+    Console.Write(i + ", ");
 }
