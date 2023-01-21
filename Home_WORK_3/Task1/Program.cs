4abe4aa0ef632dddfc8d23ea788f9442c6fa1c17
Alex <alis_mal@mail.ru>
2023-01-21 20:49:13 +0300

    Task 1 end

--- Home_WORK_3/Task1/Program.cs
+++ Home_WORK_3/Task1/Program.cs
@@ -1,9 +1,31 @@
-﻿Задача 19
+﻿/* Задача 19
 
-Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
 
 14212 -> нет
-
 12821 -> да
-
 23432 -> да
+*/
+
+int[] array = new int[5];
+
+Console.Write("Input five-digit number:");
+
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 4; i >= 0; i--) 
+{
+    array[i] = number % 10;
+    number /= 10;
+}
+
+if (array[0] == array[4] && array[1] == array[3])
+{
+    Console.WriteLine($"This number is a polydrome!");
+}
+else
+{
+    Console.WriteLine("Attention! This number is not a polydrome !");
+}
+
+
