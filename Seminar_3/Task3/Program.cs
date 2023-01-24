feaae9fea1bc4d180de6eb52c549785aacada80a
Alex <alis_mal@mail.ru>
2023-01-24 21:27:50 +0300

    Seminar 3 c#

---
+++ Seminar_3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int [] CreateArray()
+{
+    int [] Array = new int [8];
+    Random number = new Random();
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = number.Next(0,2);
+    }
+   return Array;
+}
+int [] a = CreateArray();
+
+void PrintArray (int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+   {
+     Console.Write($" {array[i]} ");
+   }
+}
+PrintArray(a);
