5f769361fe1f00a2f368b7a0ec952178c0194666
Alex <alis_mal@mail.ru>
2023-01-24 22:33:50 +0300

    Task 1 Home work 4 end

---
+++ HOME_WORK_4/Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    int resultNumber = 0;
+    while(true)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out resultNumber) && resultNumber != 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или не корректное число. Повторите ввод!\n");
+        }
+    }
+    return resultNumber;
+}
+
+int a = GetNumber("Введите число: ");
+int b = GetNumber("Введите число: ");
+Console.WriteLine($"Число {a} в натуральной степени {b} равно: {Math.Pow(a, b)}");
