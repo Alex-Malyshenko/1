5fa841edb3e75ec0c982f3aadb51e1bbb346265c
Alex <alis_mal@mail.ru>
2023-02-01 22:10:29 +0300

    End home work 6 tASK 1

---
+++ HOME_WORK_6/Task1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+
+Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+
+вводим через один Enter (разбиваем через Split())
+
+*/
+
+Console.WriteLine("Enter the numbers separated by a space: ");
+
+string numbers = Console.ReadLine();
+string[] words = numbers.Split(' ');
+int counter = 0;
+
+for (int i = 0; i < words.Length; i++)
+{
+    if (Convert.ToInt32(words[i]) > 0)
+        counter++;
+
+}
+
+Console.WriteLine($"The number of positive numbers is equal to {counter}.");
+ 
